879f2f8b1e6c2e71b88b434b5d421b06dc16371c
RyinckevichDaniil <abdul.alahakbar@gmail.com>
2020-08-04 20:17:54 +0300

    Ok, thanks

--- src/index.js
+++ src/index.js
@@ -1,83 +1,66 @@
-const testTasks = [
-    {
-        date: "Thu Jul 30 2020 20:08:50 GMT+0300 (Восточная Европа, летнее время)",
-        id: "1596128930806",
-        name: "Test1",
-        status: "Completed",
-        text: "Olo"
+const toDo = {
+    notes: [
+            {value: "Js", completed: false, id: 1596559757142},
+            {value: "World", completed: false, id: 1596559757043},
+            {value: "Hello", completed: false, id: 1596559756940},
+    ],
+
+    addNote(value) {
+        const note = {
+            value,
+            completed: false,
+            id: Date.now(),
+        };
+
+        this.notes = [note, ...this.notes];
     },
-    {
-        date: "Thu Jul 30 2020 20:08:50 GMT+0300 (Восточная Европа, летнее время)",
-        id: "1596128930807",
-        name: "Test2",
-        status: "Completed",
-        text: "oLo"
-    },
-    {
-        date: "Thu Jul 30 2020 20:08:50 GMT+0300 (Восточная Европа, летнее время)",
-        id: "1596128930808",
-        name: "Test2",
-        status: "New task",
-        text: "oLo"
-    },
-]
-const toDoList = {
-    tasks: testTasks,
-    addTask () {
-        const newTask = {
-            id: Date.now().toString(),
-            date: new Date().toString(),
-            status: confirm('Is task completed?') ? 'Completed' : 'New task' ,
-            name: prompt('Add name'),
-            text: prompt('Add some text')
-        }
-        if(!this.checkTask(newTask)) {
-            this.tasks.push(newTask)
-        }else {
-            return 'Task already exist'
-        }
-        return "added"
-    },
-    findTaskIndexById(id) {
-        return this.tasks.findIndex(function (element) {
-            return element.id === id;
-        })
-    },
-    deleteTask (id) {
-        const taskIndex = this.findTaskIndexById(id)
-        if(taskIndex >= 0) {
-            if(confirm ('Are you sure about that?')) {
-                this.tasks.splice(taskIndex, 1)
-                return this.tasks
-            }else { 
-                return 'You do not want delete this task'
-            }
-        }else {
-            return 'Id not found'
+
+    removeNote(id, confirm) {
+        if (confirm) {
+            this.notes = this.notes.filter(note => note.id !== id);
         }
     },
-    editTask(id) {
-        const taskIndex = this.findTaskIndexById(id)
-        if(taskIndex >= 0) {
-            const task = this.tasks[taskIndex],
-            status = prompt('New task status', task.status),
-            name = prompt('New task name', task.name),
-            text = prompt('New task text', task.text)
-            return task
-        } else {
-            return 'Id not found'
+
+    completeNote(id) {
+        this.notes = this.notes.map(note => ({
+            ...note,
+            completed: note.id === id ? !note.completed : note.completed
+        }))
+    },
+
+    editNote(id, value, confirm) {
+        if (confirm) {
+            this.notes = this.notes.map(note => {
+                let newNote = note;
+                
+                if (note.id === id) {
+                    newNote = {
+                        ...note,
+                        value
+                    }
+                };
+
+                return newNote;
+            });
         }
     },
-    showTasksList(){
-        const completedTask = this.tasks.filter(element => element.status === 'Completed')
-        const uncompletedTask = this.tasks.filter(element => element.status === 'New task')
-        return {
-            count: this.tasks.length,
-            completedTask,
-            uncompletedTask
-        }
-    },
-    checkTask(task) {
-        return this.tasks.find(element => element.name + element.text === task.name + task.text)
+
+    get statistic() {
+        return this.notes.reduce(
+            (acc, note) => {
+                note.completed && acc.completed++;
+                return acc;
+            },
+            { total: this.notes.length, completed: 0 }
+            );
     }
-}
+};
+
+Object.freeze(toDo);
+
+/* toDo.addNote('Hello');
+setTimeout(() => toDo.addNote('World'), 100);
+setTimeout(() => toDo.addNote('Js'), 200); */
+toDo.editNote(1596559756940, 'World', confirm('?'));
+
+console.log(toDo);
